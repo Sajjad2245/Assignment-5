c9c087575d41023eaed572f88b01f2b78d66310c
Sajjad2245 <sajjadhossain01568847066@gmail.com>
2025-08-29 21:53:01 +0600

    all thing

---
+++ script.js
@@ -0,0 +1,204 @@
+let count = 0;
+document.getElementById("hearts-1").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-2").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-3").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-4").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-5").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-6").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-7").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+document.getElementById("hearts-8").addEventListener("click" , function(){
+    const countTotal= document.getElementById('count-total').innerText;
+    let i=0;
+    while(i<1) {
+        count++;
+        i++;
+    }
+    document.getElementById('count-total').innerText = count;
+})
+
+let coin = 100;
+document.getElementById('call-1').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-1').innerText;
+const number = document.getElementById('num-1').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-2').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-2').innerText;
+const number = document.getElementById('num-2').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-3').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-3').innerText;
+const number = document.getElementById('num-3').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-4').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-4').innerText;
+const number = document.getElementById('num-4').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-5').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-5').innerText;
+const number = document.getElementById('num-5').innerText;
+let i=0;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-6').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-6').innerText;
+const number = document.getElementById('num-6').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-7').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-7').innerText;
+const number = document.getElementById('num-7').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+}
+document.getElementById('coin-count').innerText = coin;
+})
+document.getElementById('call-8').addEventListener("click" , function() {
+    const coinCount = document.getElementById('coin-count').innerText;
+const name = document.getElementById('name-8').innerText;
+const number = document.getElementById('num-8').innerText;
+if(coin<=20){
+        alert(`Not Sufficiant coin for this Call`);
+return;
+    }
+    alert(`Calling ${name} at ${number}...`);
+let i=0;
+while(i<1){
+        coin=coin-20
+         i++;
+    
+}
+document.getElementById('coin-count').innerText = coin;
+})
